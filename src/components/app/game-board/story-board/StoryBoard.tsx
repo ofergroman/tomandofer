d5ccaf0798cfb6d7bf2249d007590956da4546d6
Ofer Hopstone Groman <oferg2@brainpop.com>
2024-10-09 10:01:24 +0300

    Add minor keyboard handling changes

--- src/components/app/game-board/story-board/StoryBoard.tsx
+++ src/components/app/game-board/story-board/StoryBoard.tsx
@@ -25,6 +25,13 @@ export default function StoryBoard({className, content, updatePlayerTurn}: Story
 
     }
 
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            submitText();
+        }
+    };
+
     const inputDisabled = useMemo(() => {
         const threeWordsPattern:RegExp = /^\S+ \S+ \S+$/;
         return !threeWordsPattern.test(activeText)
@@ -35,15 +42,17 @@ export default function StoryBoard({className, content, updatePlayerTurn}: Story
             <div className='text-container w-full border-2 h-3/4 flex flex-1 text-xl p-5'>
                 <div>{submitted}</div>
                 <input
+                    tabIndex={0}
                     ref={inputRef}
                     autoFocus={true}
                     type='text'
                     value={activeText}
+                    onKeyDown={handleKeyDown}
                     onChange={(e: ChangeEvent<HTMLInputElement>) => setActiveText(e.target.value)}
                     className='ml-2 bg-transparent h-7 w-fit text-xl'
                 ></input>
             </div>
-            <button disabled={inputDisabled} onClick={submitText} className='w-56 mt-6 isabled:bg-gray-400
+            <button disabled={inputDisabled} tabIndex={1} onClick={submitText} className='w-56 mt-6 isabled:bg-gray-400
              disabled:cursor-not-allowed disabled:opacity-50'>Submit my Words</button>
         </div>
     </div>;
